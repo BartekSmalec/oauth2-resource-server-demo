dfc15e16e214979c58d11fd9aece0224fd3eefb7
bartek <smalecbartlomiej@gmail.com>
2023-02-25 19:41:04 +0100

    Init spring boot project

---
+++ src/test/java/pl/barteksmalec/oauth2resourceserverdemo/Oauth2ResourceServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.barteksmalec.oauth2resourceserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ResourceServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
